8ea8b57965631cb513d3658d3525a1a522c34c1d
Rob Relyea <rob@relyeas.net>
2022-07-11 16:20:21 -0700

    adapt test. update name in lock files

--- tests/playwright.spec.ts
+++ tests/playwright.spec.ts
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('div div')).toContainText('Hello World');
+  await expect(page.locator('div div div a h1')).toContainText('CovidSafe.fyi');
 })
