5059cd1eba4b957c80295b4d92bba0b7288920ac
Rob Relyea <rob@relyeas.net>
2022-07-13 09:35:29 -0700

    add spaces in breadcrumbbars

--- src/vaccine-time/BreadCrumbBar.js
+++ src/vaccine-time/BreadCrumbBar.js
@@ -3,9 +3,9 @@ import React from "react";
 
 function BreadCrumbBar() {
     return (
-      <div className="breadCrumbBar">
-          <a href="/"><img src="logo192.png" alt='CovidSafe.fyi' height="96" width="96" /></a> &gt; <a href="/vaccines">vaccines</a> &gt; <a href="/vaccine-time">vaccine-time</a>
-      </div>
+      <h3 className="breadCrumbBar">
+          <a href="/"><img src="logo192.png" alt='CovidSafe.fyi' height="96" width="96" /></a>&nbsp;&gt;&nbsp;<a href="/vaccines">vaccines</a>&nbsp;&gt;&nbsp;<a href="/vaccine-time">vaccine-time</a>
+      </h3>
     );
   }
   
