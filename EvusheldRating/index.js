7ce28acfd63e88d401935376bd51db65911d584e
Rob Relyea <rob@relyeas.net>
2022-07-27 10:04:55 -0700

    initial api for evusheld rating

---
+++ EvusheldRating/index.js
@@ -0,0 +1,43 @@
+const { BlobServiceClient } = require('@azure/storage-blob');
+const { v1: uuidv1} = require('uuid');
+require('dotenv').config()
+
+function getSecret() {
+    return "Some Secret";
+}
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+    const name = (req.query.name || (req.body && req.body.name));
+
+    const AZURE_STORAGE_CONNECTION_STRING = getSecret();
+
+    if (!AZURE_STORAGE_CONNECTION_STRING) {
+        throw Error("Azure Storage Connection string not found");
+    }
+
+    const blobServiceClient = BlobServiceClient.fromConnectionString(AZURE_STORAGE_CONNECTION_STRING);
+    const containerName = "evusheld-ratings-blob";
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    const blobName = "incoming/providerName" + uuidv1() + ".txt";
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+
+    console.log("\nUploading to Azure storage as blob:\n\t", blobName);
+    // Upload data to the blob
+    const data = "Hello, World!";
+    const uploadBlobResponse = await blockBlobClient.upload(data, data.length);
+    console.log(
+        "Blob was uploaded successfully. requestId: ",
+        uploadBlobResponse.requestId
+        );
+
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
